ec012b2ba7911c52ac331ae3e65de12645b90f6d
Alireza <alireza.shams2106@gmail.com>
2025-04-13 17:06:07 +0330

    docker compose

--- celerytst/celerytst/celery.py
+++ celerytst/celerytst/celery.py
@@ -2,12 +2,15 @@ import os
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "celerytst.settings") #gives celery access to project settings
-
-app = Celery("celerytst") #creating a new celery instance using name of the project
+app = Celery("celerytst")
 app.config_from_object("django.conf:settings", namespace="CELERY") #search setting for CELERY namespace where settings related to celery has it
 
 
 
-app.conf.task_routes = {'newapp.tasks.task1': {'queue':'queue1'}, 'newapp.tasks.task2': {'queue':'queue2'},}
+app.conf.task_routes = {
+    'newapp.tasks.task1': {'queue':'queue1'}, 
+    'newapp.tasks.task2': {'queue':'queue2'},
+    } #setting queue for each task
 
 app.autodiscover_tasks() #search for tasks.py in each app and tasks in there
+
--- celeryworker/celerytask.py
+++ celeryworker/celerytask.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
 app = Celery('task')
-app.config_from_object('celeryconfig')
-app.conf.imports = ('newapp.tasks')
+app.config_from_object('celeryconfig') # takes settings from celeryconfig.py file
+app.conf.imports = ('newapp.tasks') # import tasks from tasks.py file
 app.autodiscover_tasks()
