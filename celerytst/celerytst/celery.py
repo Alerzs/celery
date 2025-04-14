751fa1923b757d1387381a5bc4a29982d9b381e9
Alireza <alireza.shams2106@gmail.com>
2025-04-14 17:01:14 +0330

    Redis prioritizing

--- celerytst/celerytst/celery.py
+++ celerytst/celerytst/celery.py
@@ -5,11 +5,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "celerytst.settings") #gives cel
 app = Celery("celerytst")
 app.config_from_object("django.conf:settings", namespace="CELERY") #search setting for CELERY namespace where settings related to celery has it
 
-
-
 app.conf.task_routes = {
     'newapp.tasks.task1': {'queue':'queue1'}, 
     'newapp.tasks.task2': {'queue':'queue2'},
+    'newapp.tasks.task3': {'queue':'queue3'},
     } #setting queue for each task
 
 app.autodiscover_tasks() #search for tasks.py in each app and tasks in there
--- celerytst/newapp/tasks.py
+++ celerytst/newapp/tasks.py
@@ -1,5 +1,17 @@
 from celery import shared_task
+from time import sleep
 
 @shared_task
 def task1():
+    sleep(1)
     return
+
+@shared_task
+def task2():
+    sleep(1)
+    return
+
+@shared_task
+def task3():
+    return
+
